9f53c9ede0f90c77dbbbc00ccec92fed69516ff1
Dave Harned <dharned@gmail.com>
2017-11-09 19:20:44 -0600

    add greeter function

--- public/media/greeting.js
+++ public/media/greeting.js
@@ -2,4 +2,15 @@ var name = prompt("Hi there! What's your name?");
 
 var output = document.querySelector("#greeting");
 
-output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+if (name) {
+  output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+} else {
+  output.innerHTML = "<p>Please give us your name!</p>"
+}
+
+
+
+
+// "asdlfkjasdfjasdfkjh" === true
+//
+// "" = false
